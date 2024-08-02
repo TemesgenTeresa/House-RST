195c5b38f67e98a67f126833ee043bbfacb7189f
Temesgen <163911944+TemesgenTeresa@users.noreply.github.com>
2024-08-02 10:17:00 +0300

    House-RST project

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Navbar.css'
+
+import logo from '../../assets/Images/logo.png'
+
+const Navbar = () => {
+  return (
+    <nav className = 'container'>
+        <img src = {logo} alt ="logo not found" className = "logo" />
+        <ul>
+            <li>Home</li>
+            <li>House RST service</li>
+            <li>About Us</li>
+            <li>Language</li>
+            <li><button>Sign up</button></li>
+       </ul>
+  </nav>
+  )
+}
+
+export default Navbar
