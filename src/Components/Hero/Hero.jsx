20ed08aedc0ff74f5e2fc617d0e4c596cb26e382
Temesgen <163911944+TemesgenTeresa@users.noreply.github.com>
2024-08-02 16:39:54 +0300

    project starts and this is my to day commit

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './Hero.css'
+
 const Hero = () => {
   return (
-    <div className='hero'>
-      
+    <div className='hero container'>
+      <div className="hero-text">
+      <h1>WE ENSURE BETTER HOME FOR OUR USER</h1><br/>
+        <p>HOUSE RST is an innovative online platform
+           designed to revolutionize the way people find and utilize
+            living spaces. Our website offers a unique solution for 
+            individuals and families looking to rent, swap, or trade their homes,
+             making it easier and more efficient to find the perfect match for 
+             their housing needs.</p><br/>
+             <button className='btn'> Explore More </button>
+    </div>
     </div>
   )
 }
---
+++ src/Components/Programs/Programs.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import'./Programs.css'
+import program_1 from '../../assets/Images/Et1.jpg'
+import program_2 from '../../assets/Images/Et2.jpg'
+import program_3 from '../../assets/Images/Et3.jpg'
+import program_icon1 from '../../assets/Images/icon1.png'
+import program_icon2 from '../../assets/Images/icon2.png'
+import program_icon3 from '../../assets/Images/icon3.png'
+
+const Programs = () => {
+  return (
+    <div className='programs'>
+      <div className="program">
+        <img src={program_1} alt=""/>
+        <div className="caption">
+            <img src={program_icon1} alt=" "/>
+            <p>House Renting</p>
+    </div>
+    <div className="program">
+        <img src={program_2} alt=""/>
+        <div className="caption">
+            <img src={program_icon2} alt=" "/>
+            <p>House Swapping</p>
+    </div>
+    </div>
+    <div className="program">
+        <img src={program_3} alt=""/>
+        <div className="caption">
+            <img src={program_icon3} alt=" "/>
+            <p>House Trading</p>
+    </div>
+    </div>
+    </div>
+    </div>
+  )
+}
+
+export default Programs
