6ed69cfa83b4490ef9415b0260571133e633ab05
Temesgen <163911944+TemesgenTeresa@users.noreply.github.com>
2024-08-07 09:59:12 +0300

    adding effect on scrolling

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React,{useEffect,useState } from 'react'
 import './Navbar.css'
 
 import logo from '../../assets/Images/logo.png'
 
 const Navbar = () => {
+
+const[sticky,setSticky] = useState(false);
+useEffect(()=>{
+  window.addEventListener('scroll',()=>{
+    window.scrollY > 50 ? setSticky(true) : setSticky(false);
+  })
+}
+
+)
+
+
   return (
-    <nav className = 'container dark-nav'>
+    <nav className = {`container ${sticky? 'dark-nav' : ''}`}>
         <img src = {logo} alt ="logo not found" className = "logo" />
         <ul>
             <li>Home</li>
